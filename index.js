3ebf3aba8cd6d3db28beefea0f835603ea941c5f
Emre <ozturkemre@mail.com>
2023-06-13 02:12:54 +0300

    s13g1 tamamlandı

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+
+console.log("Hello world")
+server.listen(port, ()=>{
+    console.log("server is listening on "+ port)
+})
